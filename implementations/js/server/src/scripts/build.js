74671efea666b4bc0ee3c535b591538f6f7e1bcd
Time Kadel <time@getcaravel.fr>
2026-04-21 02:12:03 +0200

    fix(scripts/pkg): exclude binaries from export and exclude node modules from binaries

--- implementations/js/server/src/scripts/build.js
+++ implementations/js/server/src/scripts/build.js
@@ -20,10 +20,10 @@ async function run() {
     platform: 'node',
     target: 'node20',
     format: 'cjs',
-    external: [
-      '@roamhq/wrtc',
-      'systeminformation',
-    ],
+    // external: [
+    //   '@roamhq/wrtc',
+    //   'systeminformation',
+    // ],
   });
 
   console.log('📄 Updating package.json...');
